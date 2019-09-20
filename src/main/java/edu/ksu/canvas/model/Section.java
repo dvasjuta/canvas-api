ddf04026653c1ab444140d5700869164cc185b71
David Vasjuta <dvasjuta@unimelb.edu.au>
2019-09-20 11:50:18 +1000

    - added equals and hashCode

--- src/main/java/edu/ksu/canvas/model/Section.java
+++ src/main/java/edu/ksu/canvas/model/Section.java
@@ -5,6 +5,7 @@ import edu.ksu.canvas.annotation.CanvasObject;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * Class to represent Canvas course sections.
@@ -157,5 +158,34 @@ public class Section extends BaseCanvasModel implements Serializable {
 
 	public void setSortableName(String sortableName) {
 		this.sortableName = sortableName;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 59 * hash + Objects.hashCode(this.id);
+		hash = 59 * hash + Objects.hashCode(this.name);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Section other = (Section) obj;
+		if (!Objects.equals(this.name, other.name)) {
+			return false;
+		}
+		if (!Objects.equals(this.id, other.id)) {
+			return false;
+		}
+		return true;
 	}
 }
